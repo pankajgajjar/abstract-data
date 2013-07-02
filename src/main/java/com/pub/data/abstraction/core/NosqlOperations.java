6f8c8cfa6f863c23f86a0735c01f5adc777149e5
amarpotghan <amarpotghan@gmail.com>
2013-07-02 20:23:54 +0530

    initial

---
+++ src/main/java/com/pub/data/abstraction/core/NosqlOperations.java
@@ -0,0 +1,20 @@
+package com.pub.data.abstraction.core;
+
+
+public class NosqlOperations {
+	
+	
+
+	public NosqlOperations() {
+
+	}
+
+	public <T> T findAll() {
+		return null;
+	}
+	public <T> T findOne(){
+		return null;
+		
+	}
+
+}
