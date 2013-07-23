6564dd8d44cb0a19c90a45959145b201433d9fa0
amarpotghan <amarpotghan@gmail.com>
2013-07-23 20:51:19 +0530

    Added Tree Controller

---
+++ src/main/java/com/cs/web/TreeController.java
@@ -0,0 +1,12 @@
+package com.cs.web;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TreeController {
+
+	public TreeController()
+	{
+		
+	}
+}
---
+++ src/test/java/com/cs/web/TreeControllerUnitTests.java
@@ -0,0 +1,16 @@
+package com.cs.web;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TreeControllerUnitTests {
+	
+	@Before
+	public void setUp(){
+		
+	}
+	
+
+}
