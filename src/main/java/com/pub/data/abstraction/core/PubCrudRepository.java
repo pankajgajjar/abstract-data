276df385943e83e4fe02e090604272fc19def11c
amarpotghan <amarpotghan@gmail.com>
2013-07-29 13:51:22 +0530

    Clean up

--- src/main/java/com/pub/data/abstraction/core/PubCrudRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pub.data.abstraction.core;
-
-public interface PubCrudRepository {
-	
-	<T> String insert(T objectToInsert);
-	<T> T select(T query);
-	<T> T delete(T objectToDelete);
-	<E,P> P getObjectByKey(E key, Class<P> type);
-}
--- src/main/java/com/pub/data/abstraction/core/orm/ORMPlugin.java
+++
@@ -1,6 +0,0 @@
-package com.pub.data.abstraction.core.orm;
-
-public interface ORMPlugin {
-	
-
-}
